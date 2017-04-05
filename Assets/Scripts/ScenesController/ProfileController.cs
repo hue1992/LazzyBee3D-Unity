7699feb0a86a8fec0c4081be3306bf62e6f27a07
Nguyen Hue <mutang1967@gmail.com>
2017-04-05 11:10:08 +0700

    add script profile controller

---
+++ Assets/Scripts/ScenesController/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProfileController : MonoBehaviour, IScenesController
+{
+
+    public void OnBackButtonClick()
+    {
+        SceneManager.LoadScene("Home", LoadSceneMode.Additive);
+    }
+
+    public void OnLogOutButtonClick()
+    {
+        FirebaseHelper.getInstance().signOut();
+        SceneManager.LoadScene("Login");
+    }
+    public void OnLinkToFacebookButtonClick()
+    {
+        //link to facbook
+    }
+}
